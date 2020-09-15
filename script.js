a3d753b29ee045779af57b98b3386d16123128e1
SSmackz <71346549+SSmackz@users.noreply.github.com>
2020-09-15 14:30:45 -0400

    My First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var boxElement = document.body.querySelector(".box");
+
+var catElement = document.body.querySelector(".cat")
+
+var userInput = prompt("What is your name?")
+                       
+var numberOne = 8;
+var numberTwo = 2;
+
+boxElement.innerHTML=numberOne+numberTwo;
+
+catElement.innerHTML=userInput;
